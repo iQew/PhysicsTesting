d7ae00ec825c591c1cadf49095542255760871c1
ph4ntomz <kjellbunjes@gmail.com>
2020-12-18 12:56:32 +0100

    initial commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+
+    public Rigidbody2D BallRigidBody2D;
+
+    private void Awake() {
+        Application.targetFrameRate = 60;
+    }
+
+    private void Start() {
+        BallRigidBody2D.AddForce(new Vector2(1f, 10f), ForceMode2D.Impulse);
+    }
+
+}
